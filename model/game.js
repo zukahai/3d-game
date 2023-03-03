224687525bcfb61c65c5f2a5e838e81f2bfee28f
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-03-04 00:33:02 +0700

    test

--- model/game.js
+++ model/game.js
@@ -1,51 +1,162 @@
-class Cube {
-    constructor(x, y, z, color) {
-        this.geometry = new THREE.BoxGeometry(1, 1, 1);
-        this.material = new THREE.MeshBasicMaterial({ color: color });
-        this.mesh = new THREE.Mesh(this.geometry, this.material);
-        this.mesh.position.set(x, y, z);
-    }
-
-    addToScene(scene) {
-        scene.add(this.mesh);
-    }
-
-    update() {
-        this.mesh.rotation.x += 0.01;
-        this.mesh.rotation.y += 0.01;
-    }
-}
-
 class Game {
     constructor() {
-        // Khởi tạo canvas
-        this.canvas = document.createElement('canvas');
-        document.body.appendChild(this.canvas);
-
-        // Khởi tạo scene
+        // Khởi tạo renderer, camera và scene
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
         this.scene = new THREE.Scene();
-
-        // Khởi tạo camera
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        this.camera.position.z = 5;
-
-        // Khởi tạo renderer
-        this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+        // Đặt camera và renderer
         this.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(this.renderer.domElement);
+        this.camera.position.z = 10;
 
-        // Khởi tạo cube
-        this.cube = new Cube(0, 0, 0, 0x00ff00);
-        this.cube.addToScene(this.scene);
+        this.loader = new THREE.GLTFLoader();
 
-        // Render scene
-        this.render();
+        // Tạo mặt phẳng
+        // const planeGeometry = new THREE.PlaneGeometry(10, 10);
+        // const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc });
+        // const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+        // planeMesh.rotation.x = -Math.PI / 2;
+        // this.scene.add(planeMesh);
+
+        // Tạo cube
+        const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+        const cubeMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+        this.cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+        this.cubeMesh.position.set(0, 0.5, 0);
+        this.scene.add(this.cubeMesh);
+
+        this.cubeMesh.addEventListener('click', () => {
+            console.log('Clicked on cube');
+        });
+
+        this.cube = [];
+
+        for (let i = 0; i < 5; i++) {
+            let geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+            let material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+            const cube = new THREE.Mesh(geometry, material);
+            cube.position.x = (Math.random() - 0.5) * 5;
+            cube.position.y = (Math.random() - 0.5) * 5;
+            cube.position.z = (Math.random() - 0.5) * 5;
+            this.cube[i] = cube;
+            this.scene.add(cube);
+
+
+        }
+
+        // Tạo bóng
+        const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+        const sphereMaterial = new THREE.MeshPhongMaterial({ color: 0x888888 });
+        this.sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+        this.sphereMesh.position.set(1, 1, 0);
+        this.scene.add(this.sphereMesh);
+
+        // Đặt ánh sáng
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+        this.scene.add(ambientLight);
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+        directionalLight.position.set(0, 1, 1);
+        this.scene.add(directionalLight);
+
+        // Thêm sự kiện xoay cube khi ấn phím
+        document.addEventListener("keydown", (event) => {
+            if (event.key === "ArrowLeft") {
+                this.cubeMesh.position.x -= 0.1;
+            } else if (event.key === "ArrowRight") {
+                this.cubeMesh.position.x += 0.1;
+            } else if (event.key === "ArrowUp") {
+                this.cubeMesh.position.y += 0.1;
+            } else if (event.key === "ArrowDown") {
+                this.cubeMesh.position.y -= 0.1;
+            }
+        });
+
+        this.loadObject('./assets/images/abc2.glb');
+        this.hac = this.scene.children[0];
+
+
+
+        document.addEventListener('click', this.onClick.bind(this));
+
     }
 
+    loadObject(url) {
+        this.loader.load(
+            url,
+            (gltf) => {
+                // Lấy đối tượng cần hiển thị từ trong file glb
+                const object = gltf.scene;
+                // Thêm đối tượng vào scene
+                this.scene.add(object);
+                // Lưu lại đối tượng vào biến this.object
+                this.hac = object;
+            },
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+            },
+            (error) => {
+                console.error('An error happened', error);
+            }
+        );
+    }
+
+
+    onClick(event) {
+        // Lấy vị trí chuột trong khoảng viewport
+        const mouse = new THREE.Vector2(
+            (event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1
+        );
+
+        // Lấy đối tượng được click
+        const raycaster = new THREE.Raycaster();
+        raycaster.setFromCamera(mouse, this.camera);
+
+        for (let i = 0; i < this.cube.length; i++) {
+            if (this.checkClick(raycaster, this.cube[i])) {
+                this.cube[i].visible = false;
+            }
+        }
+    }
+
+    checkClick(raycaster, object) {
+        const intersects = raycaster.intersectObject(object);
+        if (intersects.length > 0) {
+            console.log('Clicked on the cube!');
+            return true;
+        }
+        return false;
+    }
+
+
+    checkCollide(object1, object2) {
+        const box1 = new THREE.Box3().setFromObject(object1);
+        const box2 = new THREE.Box3().setFromObject(object2);
+        return box1.intersectsBox(box2);
+    }
+
+    // Render scene
     render() {
-        requestAnimationFrame(() => { this.render(); });
-        this.cube.update();
+        this.cubeMesh.rotation.x += 0.01;
+        this.cubeMesh.rotation.y += 0.01;
+
+        // this.hac.scale.set(0.05, 0.05, 0.05);
+        // this.hac.rotation.x += 0.01;
+        this.hac.rotation.x = 0.3;
+        this.hac.rotation.y += 0.01;
+        for (let i = 0; i < this.cube.length; i++) {
+            this.cube[i].rotation.x += 0.01;
+            this.cube[i].rotation.y += 0.01;
+            this.cube[i].rotation.z += 0.01;
+            // this.cube[i].position.x = Math.sin(Date.now() * 0.001) * 2;
+            // this.cube[i].position.z = Math.cos(Date.now() * 0.001) * 2;
+        }
+        requestAnimationFrame(this.render.bind(this));
+        this.sphereMesh.position.x = Math.sin(Date.now() * 0.001) * 2;
+        this.sphereMesh.position.z = Math.cos(Date.now() * 0.001) * 2;
+        this.sphereMesh.position.y = Math.cos(Date.now() * 0.001) * 2;
         this.renderer.render(this.scene, this.camera);
     }
 }
 
 const game = new Game();
+game.render();
